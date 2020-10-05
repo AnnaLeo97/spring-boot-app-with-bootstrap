cf3336a3248d77f1f65205f896134b518ea71b42
AnnaLeo97 <30714027+AnnaLeo97@users.noreply.github.com>
2020-10-05 19:54:10 +0500

    Fixed 500th error

--- src/main/java/springbootapp/SpringBootAppApplication.java
+++ src/main/java/springbootapp/SpringBootAppApplication.java
@@ -1,13 +1,29 @@
 package springbootapp;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.boot.web.servlet.server.ConfigurableServletWebServerFactory;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-public class SpringBootAppApplication {
+public class SpringBootAppApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SpringBootAppApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootAppApplication.class, args);
 	}
 
+//	@Bean
+//	public WebServerFactoryCustomizer<ConfigurableServletWebServerFactory>
+//	webServerFactoryCustomizer() {
+//		return factory -> factory.setContextPath("/src/main/resources/templates");
+//	}
 }
--- src/main/java/springbootapp/config/SpringSecurityInitializer.java
+++ src/main/java/springbootapp/config/SpringSecurityInitializer.java
@@ -1,7 +1,9 @@
 package springbootapp.config;
 
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
 
+@EnableAutoConfiguration
 public class SpringSecurityInitializer extends AbstractSecurityWebApplicationInitializer {
     //пустой класс, использующийся для резистрации модуля в спринг-контейнере
 }
